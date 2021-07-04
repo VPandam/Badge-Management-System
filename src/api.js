6af2d2f6469605e9563e1e7726c3c8bf59c25044
Pandam <victorvgv96@hotmail.es>
2021-07-04 21:35:01 +0200

    --ammend

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://nervous-kirch-e2accc.netlify.app/';
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
